be953e15bbba091aa9e3b27ef7538f9864fb6749
Flavio <flavio.huanacchiri@gmail.com>
2020-11-11 03:57:10 -0500

    Feature: Vistas home, usuario

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php 
+	include __DIR__."/functions/session_helper.php";
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+			include __DIR__."/resources/views/includes/head.phtml";
+		?>
+		<title>Home</title>
+	</head>
+	<body>
+		<?php
+			include __DIR__."/resources/views/includes/header.phtml";
+		?>
+		<div class="container">
+			<h1>Página de Inicio.</h1>
+		</div>
+		<?php
+			include __DIR__."/resources/views/includes/script.phtml";
+		?>
+	</body>
+</html>
