e734ef9d205d630254d30bcbe8fb5719f7a2148a
Flavio <flavio.huanacchiri@gmail.com>
2020-11-16 20:56:16 -0500

    Fix: Update vista usuario

---
+++ js/web/usuario.js
@@ -0,0 +1,8 @@
+$('#mdlUsuario').on('show.bs.modal', function (event) {
+	var button = event.target;
+	//showLoading();
+	/*$.ajax({
+		type : "GET",
+		data : {id : }
+	});*/
+});
--- usuario.php
+++ usuario.php
@@ -16,51 +16,56 @@
 		<div class="container">
 			<?php 
 				//echo file_get_contents(__DIR__."/resources/assets/js/usuario.json"); exit;
-				$usuariosData = json_decode(file_get_contents(__DIR__."/resources/assets/js/usuario.json"), true);
+				$usuarioJson = file_get_contents(__DIR__."/resources/assets/js/usuario.json");
+				$usuariosData = json_decode($usuarioJson, true);
 			?>
 			<div class="box box-primary content-table">
 				<h1>Listado de Usuarios</h1>
 			<div class="table-responsive-md">
   				<table id="table-usuarios" class="table table-striped table-bordered nowrap" style="width:100%">
-					  <thead>
-					    <tr>
-					      <th>#</th>
-					      <th>Nombres</th>
-					      <th>Ape Paterno</th>
-					      <th>Ape Materno</th>
-					      <th>Sexo</th>
-					      <th>[]</th>
-					    </tr>
-					  </thead>
-					  <tbody>
-					  		<?php 
-					  			//print_r($usuariosData); exit;
-					    		foreach ($usuariosData as $key => $value) {
-					    	?>
+						<thead>
 						    <tr>
-						      <th><?php echo $key;?></th>
-						      <td><?php echo $value["nombres"];?></td>
-						      <td><?php echo $value["ape_paterno"];?></td>
-						      <td><?php echo $value["ape_materno"];?></td>
-						      <td><?php echo $value["sexo"];?></td>
-						      <td>
-						      	<a href="#" data-toggle="modal" data-target="#mdlUsuario" class="btn btn-primary">
-						      		<i class="fas fa-eye"></i>
-						      	</a>
-						      	<button class="btn btn-danger">
-						      		<i class="fas fa-trash"></i>
-						      	</button>
-						      </td>
+						      <th>#</th>
+						      <th>Nombres</th>
+						      <th>Ape Paterno</th>
+						      <th>Ape Materno</th>
+						      <th>Sexo</th>
+						      <th>[]</th>
 						    </tr>
-						<?php } ?>
-					  </tbody>
+						</thead>
+						<tbody>
+						  		<?php 
+						  			//print_r($usuariosData); exit;
+						    		foreach ($usuariosData as $key => $value) {
+						    	?>
+							    <tr>
+							      <th><?php echo $key;?></th>
+							      <td><?php echo $value["nombres"];?></td>
+							      <td><?php echo $value["ape_paterno"];?></td>
+							      <td><?php echo $value["ape_materno"];?></td>
+							      <td><?php echo $value["sexo"];?></td>
+							      <td>
+							      	<a href="#" data-toggle="modal" data-target="#mdlUsuario" class="btn btn-primary" data-id="<?php echo $value['id'];?>">
+							      		<i class="fas fa-eye"></i>
+							      	</a>
+							      	<button class="btn btn-danger">
+							      		<i class="fas fa-trash"></i>
+							      	</button>
+							      </td>
+							    </tr>
+							<?php } ?>
+						</tbody>
 				</table>
 			</div>
 			</div>
 			
 		</div>
+		<?php 
+			include __DIR__."/resources/views/modal/mdl_usuario.phtml";
+		?>
 		<?php
 			include __DIR__."/resources/views/includes/script.phtml";
+			include __DIR__."/resources/views/includes/loading.phtml";
 		?>
 		<script type="text/javascript">
 			var table = $('#table-usuarios').DataTable({
@@ -75,5 +80,6 @@
 			    //new $.fn.dataTable.FixedHeader( table );
 			} );
 		</script>
+		<script type="text/javascript" src="js/web/usuario.js"></script>
 	</body>
 </html>
