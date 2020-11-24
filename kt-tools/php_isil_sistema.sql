ae93a798d1090fa4df698180d070f1eea36a9a79
Flavio <flavio.huanacchiri@gmail.com>
2020-11-23 20:02:46 -0500

    Fix: Update kt-tools, bd

---
+++ kt-tools/php_isil_sistema.sql
@@ -0,0 +1,45 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.1.73 : Database - php_isil_sistema
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`php_isil_sistema` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `php_isil_sistema`;
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `ape_paterno` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `ape_materno` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sexo` enum('MASCULINO','FEMENINO') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`ape_paterno`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `usuario` */
+
+insert  into `usuario`(`id`,`nombres`,`ape_paterno`,`ape_materno`,`sexo`,`password`,`remember_token`,`created_at`,`updated_at`,`deleted_at`) values (1,'Joseph Jesus','Jimenez','Sanchez','MASCULINO','',NULL,'2020-11-23 18:34:48',NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
